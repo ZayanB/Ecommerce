5512fe66b7f25ca338e214b49f98c275299c2213
ZayanB <zayanb98@outlook.com>
2024-08-08 14:54:10 +0300

    Add to cart from homepage

--- react/src/components/FeaturedProducts.jsx
+++ react/src/components/FeaturedProducts.jsx
@@ -6,12 +6,18 @@ import { FaBalanceScale } from "react-icons/fa";
 import { SlBag } from "react-icons/sl";
 import axios from "../api/axios";
 import { useState, useEffect } from "react";
+import { notification } from "antd";
 import { parseISO, isWithinInterval, subDays } from "date-fns";
 import Spinner from "./Spinner";
+import { Link } from "react-router-dom";
 const FeaturedProducts = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [cartItem, setCartItem] = useState({
+        productid: "",
+        productprice: "",
+    });
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -29,6 +35,70 @@ const FeaturedProducts = () => {
     }, []);
 
     if (error) return <p>Error: {error}</p>;
+
+    const handleSubmit = async (cartItem, token) => {
+        try {
+            const response = await axios.post(
+                "http://127.0.0.1:8000/api/createItem",
+                cartItem,
+                {
+                    headers: {
+                        Accept: "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            notification.success({
+                message: "Success",
+                description: "Item added to cart successfully!",
+                placement: "topRight",
+                duration: 2,
+            });
+        } catch (error) {
+            notification.error({
+                message: "Error",
+                description: "Cannot add item to cart",
+                placement: "topRight",
+                duration: 2,
+            });
+        }
+    };
+
+    const addToCart = (productid, productprice) => {
+        return new Promise((resolve, reject) => {
+            const token = localStorage.getItem("access_token");
+
+            if (!token) {
+                notification.error({
+                    message: "Error",
+                    description: "Cannot add item to cart",
+                    placement: "topRight",
+                    duration: 2,
+                });
+                reject("User not authenticated");
+                return;
+            }
+
+            const newCartItem = {
+                productid: productid,
+                productprice: productprice,
+            };
+
+            setCartItem(newCartItem);
+            resolve({ cartItem: newCartItem, token });
+        });
+    };
+    const handleClick = async (productid, productprice) => {
+        try {
+            const { cartItem, token } = await addToCart(
+                productid,
+                productprice
+            );
+            handleSubmit(cartItem, token);
+        } catch (error) {
+            console.error("Error adding item to cart:", error);
+        }
+    };
 
     const isNewProduct = (createdAt) => {
         const createdDate = parseISO(createdAt);
@@ -64,21 +134,29 @@ const FeaturedProducts = () => {
                                 <div className="all-products">
                                     <figure className="figureContainer">
                                         <div className="imageContainer">
-                                            <img
-                                                key={index}
-                                                src={
-                                                    product.image &&
-                                                    product.image.length > 0
-                                                        ? product.image[0]
-                                                              .image_url
-                                                        : "https://cleversoft-moleez.myshopify.com/cdn/shop/products/moleez-product-2a.jpg?v=1524713950"
-                                                }
-                                                alt="product"
-                                                style={{
-                                                    width: "100%",
-                                                    height: "100%",
-                                                }}
-                                            />
+                                            <div className="image-size">
+                                                <Link
+                                                    to={`/allProducts/${product.product_id_pkey}`}
+                                                >
+                                                    <img
+                                                        key={index}
+                                                        src={
+                                                            product.image &&
+                                                            product.image
+                                                                .length > 0
+                                                                ? product
+                                                                      .image[0]
+                                                                      .image_url
+                                                                : "https://cleversoft-moleez.myshopify.com/cdn/shop/products/moleez-product-2a.jpg?v=1524713950"
+                                                        }
+                                                        alt="product"
+                                                        style={{
+                                                            width: "100%",
+                                                            height: "100%",
+                                                        }}
+                                                    />
+                                                </Link>
+                                            </div>
                                             <div className="overlayButtons">
                                                 <button className="overlayButton">
                                                     <GoEye className="overlayIcons" />
@@ -91,10 +169,20 @@ const FeaturedProducts = () => {
                                                 </button>
                                             </div>
                                             <div className="addToCart">
-                                                <div className="overlayCart">
-                                                    <SlBag size={20} />
-                                                </div>
-                                                ADD TO CART
+                                                <button
+                                                    className="addToCart"
+                                                    onClick={() =>
+                                                        handleClick(
+                                                            product.product_id_pkey,
+                                                            product.product_price
+                                                        )
+                                                    }
+                                                >
+                                                    <div className="overlayCart">
+                                                        <SlBag size={20} />
+                                                    </div>
+                                                    ADD TO CART
+                                                </button>
                                             </div>
                                             <div className="statusContainer">
                                                 <div
