0b7072ca2f4cc221e767c34190962c6b0a955de0
ZayanB <zayanb98@outlook.com>
2024-08-14 14:45:27 +0300

    Create reviews context

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { CartProvider } from "../Contexts/CartContext";
+import { ReviewsProvider } from "../Contexts/ReviewContext.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <CartProvider>
-            <App />
+            <ReviewsProvider>
+                <App />
+            </ReviewsProvider>
         </CartProvider>
     </React.StrictMode>
 );
