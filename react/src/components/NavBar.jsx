b0684588e76faecf67f5cb7019e3537d507af1b2
ZayanB <zayanb98@outlook.com>
2024-08-21 12:15:50 +0300

    responsive enhance

--- react/src/components/NavBar.jsx
+++ react/src/components/NavBar.jsx
@@ -14,20 +14,20 @@ const NavBar = () => {
 
     const screenWidth = useScreenWidth();
     const mainContainerClass =
-        screenWidth < 800
+        screenWidth < 986
             ? "MainContainerNavBar-mobile-nav"
             : "MainContainerNavBar-desktop-nav";
     const listContainerClass =
-        screenWidth < 900
+        screenWidth < 800
             ? "ListContainer-mobile-list"
             : "ListContainer-desktop-list";
-    const ulClass = screenWidth < 768 ? "mobile-ul" : "desktop-ul";
-    const arrowDownClass = screenWidth < 768 ? "mobile-arrow" : "desktop-arrow";
-    const ShopName = screenWidth < 768 ? "ShopName-mobile" : "ShopName-desktop";
+    const ulClass = screenWidth < 800 ? "mobile-ul" : "desktop-ul";
+    const arrowDownClass = screenWidth < 800 ? "mobile-arrow" : "desktop-arrow";
+    const ShopName = screenWidth < 800 ? "ShopName-mobile" : "ShopName-desktop";
     return (
         <nav className={mainContainerClass}>
             <div
-                className={screenWidth > 800 ? "hide-display" : "show-nav-list"}
+                className={screenWidth > 986 ? "hide-display" : "show-nav-list"}
             >
                 <NavListPanel />
             </div>
@@ -48,7 +48,7 @@ const NavBar = () => {
             </div>
             <div
                 className={
-                    screenWidth < 800 ? "hide-display" : "center-nav-list"
+                    screenWidth < 986 ? "hide-display" : "center-nav-list"
                 }
             >
                 <NavbarList />
@@ -56,13 +56,13 @@ const NavBar = () => {
 
             <div>
                 <div className="navbar-addToCart">
-                    <div className={screenWidth > 800 ? "" : "nav-user-hide"}>
+                    <div className={screenWidth > 986 ? "" : "nav-user-hide"}>
                         <UserDropDown />
                     </div>
                     <div>
                         <PiScales
                             className={
-                                screenWidth > 800
+                                screenWidth > 986
                                     ? "CartHover"
                                     : "nav-user-hide"
                             }
@@ -71,7 +71,7 @@ const NavBar = () => {
                     <div
                         style={{
                             transform:
-                                screenWidth < 800 ? "translateY(5px)" : "",
+                                screenWidth < 986 ? "translateY(5px)" : "",
                         }}
                     >
                         <CartPanel />
